ef19a5383d3ae31985de603313e813015a839ea6
Vimukthi Gunasekara <vimukthisadaruwan65@gmail.com>
2023-02-19 11:31:39 -0500

    add questions to js file

---
+++ assets/js/script.js
@@ -0,0 +1,43 @@
+var questions = [{
+    question: "Which statement creates a new object using the Person constructor? Which statement creates a new Person object called 'student'?",
+    answer1: "var student = new Person();",
+    answer2: "var student = construct Person;",
+    answer3: "var student = Person();",
+    answer4: "var student = construct Person();",
+    correct: "var student = new Person();"
+}, {
+    question: "Which operator returns true if the two compared values are not equal?",
+    answer1: "<>",
+    answer2: "==!",
+    answer3: "~",
+    answer4: "!==",
+    correct: "!=="
+}, {
+    question: "Which property references the DOM object that dispatched an event?",
+    answer1: "self",
+    answer2: "object",
+    answer3: "source",
+    answer4: "target",
+    correct: "target"
+}, {
+    question: "Which method converts JSON data to a JavaScript object?",
+    answer1: "JSON.fromString();",
+    answer2: "JSON.toObject()",
+    answer3: "JSON.parse();",
+    answer4: "JSON.stringify()",
+    correct: "JSON.parse();"
+}, {
+    question: "Which Object method returns an iterable that can be used to iterate over the properties of an object?",
+    answer1: "Object.keys()",
+    answer2: "Object.each()",
+    answer3: "Object.loop()",
+    answer4: "Object.get()",
+    correct: "Object.keys()"
+}, {
+    question: "Which of the following values is not a Boolean false?",
+    answer1: "Boolean(false)",
+    answer2: "Boolean(NaN)",
+    answer3: "Boolean('')",
+    answer4: "Boolean(0)",
+    correct: "Boolean(false)"
+}];
